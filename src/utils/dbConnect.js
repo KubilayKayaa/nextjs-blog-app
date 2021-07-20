1e668e430e18918317f40d230177404b202aac6d
Kubilay Kaya <kkubilay24@gmail.com>
2021-07-20 22:23:48 +0300

    initial
    initial

--- src/utils/dbConnect.js
+++ src/utils/dbConnect.js
@@ -6,13 +6,10 @@ async function dbConnect() {
   if (connection.isConnected) {
     return;
   }
-  const db = await mongoose.connect(
-    "mongodb+srv://zkubilaykaya:115599ka@nodeblog.5nnky.mongodb.net/posts?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  );
+  const db = await mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
   connection.isConnected = db.connections[0].readyState;
   console.log(connection.isConnected);
