d6b36c35f25c917fc86792ff4be14cbffd1223b0
Kubilay Kaya <kkubilay24@gmail.com>
2021-07-20 22:41:07 +0300

    initial
    initial

--- src/utils/dbConnect.js
+++ src/utils/dbConnect.js
@@ -6,10 +6,13 @@ async function dbConnect() {
   if (connection.isConnected) {
     return;
   }
-  const db = await mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  const db = await mongoose.connect(
+    "mongodb+srv://zkubilaykaya:mongodb123@nodeblog.5nnky.mongodb.net/posts?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
 
   connection.isConnected = db.connections[0].readyState;
   console.log(connection.isConnected);
