9d2fc008270416dc888a80b0322e244dca76a786
Kubilay Kaya <kkubilay24@gmail.com>
2021-07-22 15:53:37 +0300

    initial
    initial

--- http-config.js
+++ http-config.js
@@ -1,7 +1,7 @@
 //https://nextjs-sample-blog-app.vercel.app
 //http://localhost:3000
 
-const http = "http://localhost:3000";
+const http = "https://nextjs-sample-blog-app.vercel.app";
 export const mongoURI =
   "mongodb+srv://devadmin:devadmin123@cluster0.wgqzk.mongodb.net/newBlog?retryWrites=true&w=majority";
 
